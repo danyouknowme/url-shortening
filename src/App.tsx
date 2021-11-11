c1583e063f08345526a2171a78c5a81502360d6a
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-12 01:15:43 +0700

    add navbar and menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
 
 const App = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
   return (
     <div className="App">
-      
+      <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
+      <Menu openMenu={openMenu} setOpenMenu={setOpenMenu} />
     </div>
   );
 }
